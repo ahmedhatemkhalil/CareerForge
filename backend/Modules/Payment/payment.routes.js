169ef90f43c074500e0e972e42ee6206a3cb341f
elaf <elaf6369@gmail.com>
2026-06-26 01:38:39 +0300

    feat: implement payment processing and subscription management features

--- backend/Modules/Payment/payment.routes.js
+++ backend/Modules/Payment/payment.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { verifyToken } from "../../middleware/auth.js";
-import {createCheckoutSession, getSubscription} from "./payment.controller.js";
+import { verifyToken, adminOnly } from "../../middleware/auth.js";
+import {createCheckoutSession, getSubscription, createPortalSession, getMyPayments, getAllPayments, getPaymentById} from "./payment.controller.js";
 const router = express.Router();
 router.post("/create-checkout-session", verifyToken, createCheckoutSession);
 router.get("/subscription", verifyToken, getSubscription);
+router.post("/create-portal-session", verifyToken, createPortalSession);
+router.get("/my-payments", verifyToken, getMyPayments);
 
+
+router.get("/",verifyToken, adminOnly, getAllPayments);
+router.get("/:id", verifyToken, adminOnly, getPaymentById);
 export default router;
